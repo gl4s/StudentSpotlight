e5d3332bab6c25148a4e8f8bbdcf2df502fb7d15
FrechSzabi <frechszabolcs@gmail.com>
2024-02-29 14:02:55 +0100

    ad

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -10,40 +10,38 @@ const MainPage = () => {
     return (
 
       <div className="container main-container">
-        <Navbar />
-        {/* <div className="content-box"> */}
-          <div className="row">
-            <div className="col-md-4">
-              <div className="section-box">
-              <p className="label">STUDENT PAGE</p>
-                <img src="../../media/pexels-andrea-piacquadio-3768126.jpg" alt="Student Banner" className="mini-banner" />
-                <Link to="/studentlogin" className="btn btn-primary custom-button">
-                  Login
-                </Link>
-              </div>
+            <Navbar />
+            <div className="row">
+                <div className="col-sm-12 col-md-4">
+                    <div className="section-box">
+                        <p className="label">STUDENT PAGE</p>
+                        <img src="../../media/pexels-andrea-piacquadio-3768126.jpg" alt="Student Banner" className="mini-banner" />
+                        <Link to="/studentlogin" className="btn btn-primary custom-button">
+                            Login
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-md-4">
+                    <div className="section-box">
+                        <p className="label">TEACHER PAGE</p>
+                        <img src="../../media/pexels-christina-morillo-1181534.jpg" alt="Teacher Banner" className="mini-banner" />
+                        <Link to="/teacherlogin" className="btn btn-primary custom-button">
+                            Login
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-md-4">
+                    <div className="section-box">
+                        <p className="label"> SCHOOL REGISTRATION</p>
+                        <img src="../../media/pexels-pixabay-256467.jpg" alt="School Registration Banner" className="mini-banner" />
+                        <Link to="/schoolregistration" className="btn btn-primary custom-button">
+                            Registration
+                        </Link>
+                    </div>
+                </div>
             </div>
-  
-            <div className="col-md-4">
-              <div className="section-box">
-              <p className="label">TEACHER PAGE</p>
-                <img src="../../media/pexels-christina-morillo-1181534.jpg" alt="Teacher Banner" className="mini-banner" />                
-                <Link to="/teacherlogin" className="btn btn-primary custom-button">
-                  Login
-                </Link>
-              </div>
-            </div>
-  
-            <div className="col-md-4">
-              <div className="section-box">
-              <p className="label"> SCHOOL REGISTRATION</p>
-              <img src="../../media/pexels-pixabay-256467.jpg" alt="School Registration Banner" className="mini-banner" />
-                <Link to="/schoolregistration" className="btn btn-primary custom-button">
-                  Registration
-                </Link>
-              </div>
-            </div>
-          {/* </div> */}
-        </div>
         <Footer />
       </div>
     );
